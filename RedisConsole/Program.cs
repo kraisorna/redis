178292b2795a213f9e71c52b91bb767ff470a946
Franky Yan <kraisornar@gmail.com>
2018-01-19 23:09:44 +0700

    init code

---
+++ RedisConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using ServiceStack.Redis;
+
+namespace RedisConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var redisManager = new RedisManagerPool("localhost:6379");
+            var redis = redisManager.GetClient();
+
+            using (StreamReader r = new StreamReader("example.json"))
+            {
+                string json = r.ReadToEnd();
+                //Console.WriteLine(json);
+                //redis.SetValue("json",json);
+            }
+
+            var x1 = redis.GetValue("foo");
+            Console.WriteLine(x1);
+
+            var x2 = redis.GetValue("json");
+            Console.WriteLine(x2);
+
+            Console.ReadLine();
+        }
+    }
+}
